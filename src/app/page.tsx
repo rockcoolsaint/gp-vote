6863624f5b3c179503bbe65b5841d7dc9fdc5f17
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-11-05 01:20:19 +0100

    metamask integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,16 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const Homepage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to a specific page
+    router.push('/admin'); // Replace '/target-page' with your desired path
+  }, [router])
+  
   return (
     <div className=''>Homepage</div>
   )
